fb550a4d4756c2067959121561566af9286e3f58
MomontD <momontd@gmail.com>
2023-02-28 20:44:45 +0200

    Lesson1 1/2 set1.union video 1:20

---
+++ lesson1_arr_func_str_mothods.py
@@ -0,0 +1,16 @@
+
+
+users = {
+    'name' : 'Vasya',
+    'age'  : 22,
+    'city' : 'Los angeles'
+}
+
+st='12312312'
+print(set(st))
+
+set1={1,2,3,4,5}
+set2={10,9,7}
+
+
+print(set1.isdisjoint(set2))
