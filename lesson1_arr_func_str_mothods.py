6bbf7cf63c493271f58bdb0c7506729837b4608f
MomontD <momontd@gmail.com>
2023-03-02 11:30:12 +0200

    HM 80%

--- lesson1_arr_func_str_mothods.py
+++ lesson1_arr_func_str_mothods.py
@@ -1,16 +1,129 @@
+# strings
+# 1)написати прогу яка вибирає зі введеної строки цифри і виводить їх через кому,
+# наприклад:
+# st = 'as 23 fdfdg544' введена строка
+# 2,3,5,4,4        #вивело в консолі.
+
+st = 'as 23 fdfdg544'
+digit_st=''
+
+for el in st :
+    if el.isdigit() :
+        digit_st += el + ','
+print(digit_st.rstrip(','))
+
+# рішення з курсів
+# digit_str = ','.join([char for char in st if char.isdigit()])
+# print(digit_str)
+
+# #################################################################################
+# 2)написати прогу яка вибирає зі введеної строки числа і виводить їх
+# так як вони написані
+# наприклад:
+#   st = 'as 23 fdfdg544 34' #введена строка
+#   23, 544, 34              #вивело в консолі
 
 
-users = {
-    'name' : 'Vasya',
-    'age'  : 22,
-    'city' : 'Los angeles'
-}
+st = 'as 23 fdfdg544 34 a'
+arr=[]
+digit_element=''
 
-st='12312312'
-print(set(st))
+for el in st :
+    if el.isdigit() :
+        digit_element+=el
+    if el == ' ' and digit_element != '' :
+        arr.append(digit_element)
+        digit_element=''
+print(','.join(arr))
 
-set1={1,2,3,4,5}
-set2={10,9,7}
+# рішення з курсів :
+# res =','.join(''.join([el if el.isdigit() else ' ' for el in st ]).split())
+# print(res)
 
+# #################################################################################
+#
+# list comprehension
+#
+# 1)є строка:
+# greeting = 'Hello, world'
+# записати кожний символ як окремий елемент списку і зробити його заглавним:
+# ['H', 'E', 'L', 'L', 'O', ',', ' ', 'W', 'O', 'R', 'L', 'D']
 
-print(set1.isdisjoint(set2))
+greeting = 'Hello, world'
+res = [el.upper() for el in greeting]
+print(res)
+
+# 2) з диапозону від 0-50 записати тільки не парні числа при цьому піднести їх до квадрату
+# приклад:
+# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, ...]
+
+res = [pow(el,2) for el in range(0,50) if el%2 ]
+print(res)
+
+# рішення з курсів :
+# print([i ** 2 for i in range(50) if i % 2])
+
+# #################################################################################
+# function
+#
+# - створити функцію яка виводить ліст
+array = [1,2,3,4,5]
+def print_array (in_array) :
+    return in_array
+
+print(print_array(array))
+
+# - створити функцію яка приймає три числа та виводить та повертає найбільше.
+def max_number (num1,num2,num3) :
+    print("Отримані числа числа : ",num1,num2,num3 ,sep=' ')
+    return max(num1,num2,num3)
+
+print('Найбільше число : ',max_number(3,7,5))
+
+# - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+def min_max_num(*args) :
+    print('Найбільше число :' , max(*args))
+    return min(*args)
+
+print('Найменше число : ',min_max_num(1, 2, 6, 9, 4, 1, 15))
+# - створити функцію яка повертає найбільше число з ліста
+# - створити функцію яка повертає найменьше число з ліста
+arr_of_nums = [1,4,7,45,23,43,7,55]
+def min_max_num_list (list) :
+    min_num= min(list)
+    max_num= max(list)
+    return (min_num,max_num)
+
+print(min_max_num_list(arr_of_nums))
+# - створити функцію яка приймає ліст чисел та складає значення елементів ліста та повертає його.
+# - створити функцію яка приймає ліст чисел та повертає середнє арифметичне його значень.
+arr_of_nums = [1,4,7,45,23,43,7,55]
+def calc_el_list(list) :
+    sum_list = sum(list)
+    average_num = sum_list/len(list)
+    return (sum_list,average_num)
+
+print(calc_el_list(arr_of_nums))
+
+# ################################################################################################
+# 1)Дан list:
+#   list = [22, 3,5,2,8,2,-23, 8,23,5]
+#   - знайти мін число
+#   - видалити усі дублікати
+#   - замінити кожне 4-те значення на 'X'
+
+list = [22, 3,5,2,10,2,-23, 8,23,5]
+
+def actions_of_list (list) :
+    min_num = min(list)
+    dub_of  = set(list)
+    for i in range(3,len(list),4) :
+        list[i]='X'
+
+    return (min_num,dub_of,list)
+
+print(actions_of_list(list))
+
+# 2) вивести на екран пустий квадрат з "*" сторона якого вказана як агрумент функції
+# 3) вывести табличку множення за допомогою цикла while
+# 4) переробити це завдання під меню
