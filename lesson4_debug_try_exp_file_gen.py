10dded0073bf39e93557d4c3331d45253d07f211
MomontD <momontd@gmail.com>
2023-03-18 09:08:33 +0200

    HW lesson 4 done 1,2 - ok , additional 3 - none

--- lesson4_debug_try_exp_file_gen.py
+++ lesson4_debug_try_exp_file_gen.py
@@ -2,7 +2,6 @@
 # 1) Є ось такий файл... ваша задача записати в новий файл тільки email'ли з доменом gmail.com'
 #   '(Хеш то що з ліва записувати не потрібно)
 
-# Читання з файлу + фільтрація адресів по @gmail.com
 try:
     with open('emails.txt', 'r') as file:
 
@@ -15,8 +14,6 @@ try:
 
 except Exception as err:
     print(err)
-
-# Запис у файл фдресів '@gmail.com'
 
 try:
     with open('emails_gmail.txt', 'w') as file:
@@ -46,7 +43,8 @@ class Get_shop:
         self.coast = coast
         self.id = id
 
-def write_file(file_name,list):
+
+def write_file(file_name, list):
     try:
         with open(file_name, 'w') as file:
             json.dump(list, file)
@@ -55,18 +53,35 @@ def write_file(file_name,list):
         print(err)
 
 
-
 def read_file(file_name):
+
+    data = []
+
     try:
         with open(file_name, 'r') as file:
             data = json.load(file)
 
     except Exception as err:
-        write_file('shopping_book.txt',shopping_list)
+        write_file('shopping_book.txt', data)
 
     return data
 
-shopping_list = []
+def search (position, key, list):
+
+    res = ''
+    find_index = int
+
+    for index, el in enumerate(list):
+        if el[key] == position:
+            res = el
+            find_index = index
+            print('Знайдено позицію :', res)
+    if res == '':
+        print('Позицію не знайдено')
+
+    return find_index
+
+shopping_list : list[dict] = []
 choice = '0'
 
 while choice != '6':
@@ -95,12 +110,12 @@ while choice != '6':
                 if len(shopping_list) == 0:
                     product_id = 1
                 else:
-                    product_id = product_id + shopping_list[-1]['id']
+                    product_id = shopping_list[-1]['id'] + 1     # розраховуємо id запису (id попер. запису + 1)
 
                 shopping_element = Get_shop(product_name, product_coast, product_id)
                 shopping_list.append(shopping_element.__dict__)
 
-                write_file('shopping_book.txt',shopping_list)
+                write_file('shopping_book.txt', shopping_list)
 
                 choice_shop = input('Додати ще купівлю ? (y/n) : ')
 
@@ -108,5 +123,101 @@ while choice != '6':
 
             print(shopping_list)
 
+        case '3':
+
+            print('Вкажіть параметр по якому здійснити пошук : \n'
+                  '1 - за назвою    \n'
+                  '2 - за вартістю  \n'
+                  '3 - за id ')
+
+            user_choice = input('Введіть Ваш вибір : ')
+
+            if user_choice == '1':
+                element = input('Введіть назву купівлі: ')
+                search(element, 'name', shopping_list)
+
+            elif user_choice == '2':
+                element = int(input('Введіть вартість купівлі: '))
+                search(element, 'coast', shopping_list)
+
+            elif user_choice == '3':
+                element = int(input('Введіть номер id: '))
+                search(element, 'id', shopping_list)
+
+            else: print('Ви ввели невірний параметр , здійснено перехід на головне меню.')
+
+        case '4':
+            coasts_array = [el['coast'] for el in shopping_list if el['coast']]
+
+            search(max(coasts_array), 'coast', shopping_list)
+
+        case '5':
+            user_delete_choice = int(input('Введіть номер id для видалення : '))
+
+            del_index = search(user_delete_choice, 'id', shopping_list)
+            shopping_list.pop(del_index)
+            write_file('shopping_book.txt', shopping_list)
+
         case _:
-            pass
+            print('Ви ввели не вірний пункт меню')
+
+############################################################################################################
+# !!! Не робоче , не превіряти
+
+# потрібно брати по черзі с кожного списку id і класти в список res, якщо таке значення вже є в результуючому списку то
+# брати наступне з того ж підсписку
+#
+# в результат має записатись тільки 5 id
+#
+# з даним списком мае вийти ось такий результат:
+# res = [1110, 1120, 1130, 1111, 1122]
+
+# data = [
+#     [
+#         {"id": 1110, "field": {}},
+#         {"id": 1111, "field": {}},
+#         {"id": 1112, "field": {}},
+#         {"id": 1113, "field": {}},
+#         {"id": 1114, "field": {}},
+#         {"id": 1115, "field": {}},
+#     ],
+#     [
+#         {"id": 1110, "field": {}},
+#         {"id": 1120, "field": {}},
+#         {"id": 1122, "field": {}},
+#         {"id": 1123, "field": {}},
+#         {"id": 1124, "field": {}},
+#         {"id": 1125, "field": {}},
+#
+#     ],
+#     [
+#         {"id": 1130, "field": {}},
+#         {"id": 1131, "field": {}},
+#         {"id": 1122, "field": {}},
+#         {"id": 1132, "field": {}},
+#         {"id": 1133, "field": {}},
+#
+#     ]
+# ]
+#
+# list1, list2, list3 = [*data]
+# res = []
+# iteration_loop = max(len(list1), len(list2), len(list3))
+# count = 0
+#
+# while count <= iteration_loop:
+#
+#     if count < len(list1):
+#         if list1[count]['id'] not in res :
+#             res.append(list1[count]['id'])
+#     if count < len(list2):
+#         if list2[count]['id'] not in res :
+#             res.append(list2[count]['id'])
+#     if count < len(list3):
+#         if list3[count]['id'] not in res :
+#             res.append(list3[count]['id'])
+#
+#     count+=1
+#
+# print(res)
+
